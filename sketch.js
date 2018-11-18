70ea000ca417a386c757f8964e69bcd8b2f4838d
Davidemacchipoli <davidemacchi97@gmail.com>
2018-11-18 16:19:10 +0100

    Action Painting

--- sketch.js
+++ sketch.js
@@ -1,43 +1,73 @@
-var r = 0;
-var g = 0;
-var b = 0;
-var bgColor;
-var myTurnedAxis = 'None';
+
+var posX;
+var posY;
+var value1;
+var value2;
+var value3;
+var img;
+
+
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	textSize(18);
-  rectMode(CENTER);
+
+    background(100);
+  createCanvas(windowWidth,windowHeight);
+//generateBall();
+textFont('Sedgwick Ave');
+textAlign(CENTER);
+textSize(80);
+text('Digital Action Painting', windowWidth/2, windowHeight/9-20);
+textSize(50);
+text('Create your screen saver', windowWidth/2, windowHeight/6 + 5);
+textSize(30);
+text('Tap the screen to start painting, keep on tapping to change color', windowWidth/2, (windowHeight/6) + 60);
+text('Move your smartphone to color the screen', windowWidth/2, (windowHeight/6) + 100);
+rectMode(CENTER);
+
+  img = loadImage("assets/iphone-png-8.png");
+//noFill();
+//rect(width/2, height/2 +140, 900/2, 1600/2)
+//noFill();
+//rect(width/2, height/2 +180, 900/1.7, 1600/1.7)
 }
 
 function draw() {
-  bgColor = color(r,g,b);
-  background(bgColor);
-  fill(255);
-  noStroke();
-  text('Click in the center to enable full screen', 5, 20);
-  text(myTurnedAxis, 5, 40);
-  stroke(255);
-  noFill();
-  rect(width/2,height/2,width/2,height/2);
-  if (r >= 5) {
-    r -= 5;
+
+movingman();
+image(img, windowWidth/4 - 50, windowHeight/4, img.width/2, img.height/2);
+
+}
+
+function movingman() {
+
+var posX = map (rotationY, -60,90,0, windowHeight);
+var posY = map (rotationX, -45,45,0, windowWidth);
+fill(value1,value2,value3);
+noStroke();
+ellipse(posX, posY, 30,30);
+ }
+
+//function generateBall(){
+  //for (var i = 0; i < 5; i++) {
+    //ellipse(random(0, width), random(0, height), 50);
+  //}
+//}
+
+
+function touchStarted() {
+  if (value1 === 0) {
+    value1 = 0
+  } else {
+    value1 = random()*255;
+  };
+  if (value2 === 0) {
+    value2 = 0
+  } else {
+    value2 = random()*255;
+  };
+  if (value3 === 0) {
+    value3 = 0
+  } else {
+    value3 = random()*255;
   }
-}
-
-
-function deviceTurned(){
-  if (turnAxis == 'X') {
-    r = 255;
-  }
-}
-function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
-}
-
-function mousePressed() {
-	if(mouseX > width/4 && mouseX < width/4*3 && mouseY > height/4 && mouseY < height/4*3) {
-		var fs = fullscreen();
-		fullscreen(!fs);
-	}
 }
